7b060780e86a16e25dbe9f85a47b084d6bc742d6
Smudgy <110081190+ItsSmudgy@users.noreply.github.com>
2022-08-09 13:46:55 -0600

    Add files via upload

---
+++ Content/NPCs/SpiritOfLight/Projectiles/CrystalShockwave.cs
@@ -0,0 +1,29 @@
+﻿using Fandomonium.Content.Projectiles.BaseProjectileClasses;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+
+namespace Fandomonium.Content.NPCs.SpiritOfLight.Projectiles
+{
+    public class CrystalShockwave : BaseVisualExplosionProjectile
+    {
+        public override string Texture => "Fandomonium/Assets/Textures/BloomPulse";
+        public override int TimeLeft => 60;
+        public override float MaxScale => Projectile.ai[1];
+        public override Color GetShockwaveColor => Color.Lerp(new Color(255, 141, 211), new Color(169, 27, 125), Main.rand.NextFloat(0.1f, 0.9f));
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Crystal Shockwave");
+            ProjectileID.Sets.DrawScreenCheckFluff[Projectile.type] = 10000;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = Projectile.height = 96;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.penetrate = -1;
+        }
+    }
+}
---
+++ Content/NPCs/SpiritOfLight/Projectiles/CrystalShockwave2.cs
@@ -0,0 +1,36 @@
+﻿using Fandomonium.Content.Projectiles.BaseProjectileClasses;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+
+namespace Fandomonium.Content.NPCs.SpiritOfLight.Projectiles
+{
+    public class CrystalShockwave2 : BaseVisualExplosionProjectile
+    {
+        public override string Texture => "Fandomonium/Assets/Textures/BlankSprite";
+        public override int TimeLeft => 200;
+        public override float MaxScale => 15f;
+
+        public override bool UsePillarShieldShader => true;
+        public override Color GetShockwaveColor => Color.Lerp(Color.White, new Color(255, 141, 211), 0.85f);
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Insurgency Shockwave");
+            ProjectileID.Sets.DrawScreenCheckFluff[Projectile.type] = 10000;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = Projectile.height = 2;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.penetrate = -1;
+        }
+
+        public override void PostAI()
+        {
+            MaxRadius = 350f;
+        }
+    }
+}
