f5f534eb031cddb79e489ea159db14e8a7812b8e
Smudgy <110081190+ItsSmudgy@users.noreply.github.com>
2022-08-09 14:17:15 -0600

    Add files via upload

---
+++ Content/Projectiles/Crystal_Spike.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Fandomonium.Content.Dusts;
+
+namespace Fandomonium.Content.Projectiles
+{
+    internal class Crystal_Spike : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 24;
+            Projectile.height = 18;
+            Projectile.friendly = true;
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = true;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.aiStyle = ProjectileID.CrystalDart;
+            Projectile.penetrate = 5;
+        }
+    }
+}
---
+++ Content/Projectiles/Spark.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Fandomonium.Content.Dusts;
+
+namespace Fandomonium.Content.Projectiles
+{
+    internal class Spark : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 52;
+            Projectile.height = 52;
+
+            Projectile.friendly = true;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+
+            Projectile.DamageType = DamageClass.Magic;
+
+            Projectile.aiStyle = -1;
+
+            Projectile.penetrate = -1;
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            base.OnHitNPC(target, damage, knockback, crit);
+            target.AddBuff(BuffID.OnFire3, 600);
+        }
+
+        public override void AI()
+        {
+            Projectile.ai[0]++;
+            if(Projectile.ai[0] < 60f)
+            {
+                Projectile.velocity *= 1.01f;
+            } else
+            {
+                Projectile.velocity *= 1.05f;
+                if(Projectile.ai[0] >= 180)
+                {
+                    Projectile.Kill();
+                }
+            }
+
+            Lighting.AddLight(Projectile.Center, 0.75f, 0.75f, 0.75f);
+
+            if(Main.rand.NextBool(2))
+            {
+                int numToSpawn = Main.rand.Next(3);
+                for(int i = 0; i < numToSpawn; i++)
+                {
+                    Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<Ember>(), Projectile.velocity.X * 0.1f, Projectile.velocity.Y * 0.1f,
+                        0, default(Color), 1f);
+                }
+            }
+        }
+    }
+}
